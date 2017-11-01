45466ef4a6071b3b00cb72650a661479e5679698
wenchen0511 <wc21511@163.com>
2017-11-01 23:23:41 +0800

    添加日志，修改mybatis为xml形式。

--- src/main/java/com/xianqu/mapper/UserMapper.java
+++ src/main/java/com/xianqu/mapper/UserMapper.java
@@ -1,16 +1,9 @@
 package com.xianqu.mapper;
 
 import com.xianqu.entity.UserEntity;
-import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 public interface UserMapper {
-    @Select("SELECT * FROM my_info")
-    @Results({
-        @Result(property = "id",  column = "id"),
-        @Result(property = "name", column = "name"),
-        @Result(property = "phoneNumber", column = "phone_number")
-    })
     List<UserEntity> getAll();
 }
